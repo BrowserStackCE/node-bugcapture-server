4d8aaa2390ce07617b83b790e805b9ef752fd34a
Kenton Self <kenton.s@browserstack.com>
2024-12-06 20:59:02 -0600

    fix for command arg

--- bugcapture-server.js
+++ bugcapture-server.js
@@ -93,6 +93,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(port, () => {
+  console.log("Server running on port " +  port);
 });
